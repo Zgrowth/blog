6682f568ba14df5eda68f4b5aa2ea82e67ade821
Zgrowth <141380129+Zgrowth@users.noreply.github.com>
2024-06-04 17:15:27 +0800

    feat:初始化博客应用

---
+++ .vuepress/config.js
@@ -0,0 +1,71 @@
+module.exports = {
+    "title": "随身携带的记事本",
+    "description": "在喧嚣的城市角落，沐浴晨光或陷入黄昏的余辉，每一步行走，都是一段未完待续的旋律。我们随身携带记事本，将梦想和现实的交响曲，通过文字和墨迹，谱写成一首首平凡中的不平凡",
+    "dest": "dist",
+    "head": [
+      [
+        "link",
+        {
+          "rel": "icon",
+          "href": "/favicon.ico"
+        }
+      ],
+      [
+        "meta",
+        {
+          "name": "viewport",
+          "content": "width=device-width,initial-scale=1,user-scalable=no"
+        }
+      ]
+    ],
+    "base": '/blog/',
+    "theme": "reco",
+    "themeConfig": {
+      "nav": [
+        {
+          "text": "首页",
+          "link": "/",
+          "icon": "reco-home"
+        },
+        {
+          "text": "时间轴",
+          "link": "/timeline/",
+          "icon": "reco-date"
+        },
+        {
+          text: 'GitHub',
+          link: 'https://github.com/Zgrowth',
+          icon: 'reco-github'
+        }
+      ],
+      "type": "blog",
+      "blogConfig": {
+        "category": {
+          "location": 2,
+          "text": "分类"
+        },
+        "tag": {
+          "location": 3,
+          "text": "标签"
+        }
+      },
+      "friendLink": [
+        /* {
+          "title": "午后南杂",
+          "desc": "Enjoy when you can, and endure when you must.",
+          "email": "1156743527@qq.com",
+          "link": "https://www.recoluan.com"
+        }, */
+      ],
+      "logo": "/avatar.png",
+      "search": true,
+      "searchMaxSuggestions": 10,
+      "lastUpdated": "Last Updated",
+      "author": "zgrowth",
+      "authorAvatar": "/avatar.png",
+      "startYear": "2024",
+    },
+    "markdown": {
+      "lineNumbers": true
+    },
+  }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# blog
-随身携带的记事本
+---
+home: true
+# heroImage: /hero.png
+heroImageStyle: {
+  maxWidth: '600px',
+  width: '100%',
+  display: block,
+  margin: '9rem auto 2rem',
+  background: '#fff',
+  borderRadius: '1rem',
+}
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
+---
